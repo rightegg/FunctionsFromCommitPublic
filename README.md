4516250714e127738fda8268c095e9f7af1b416c
Richard Fang <richardfang2005@gmail.com>
2022-08-29 00:41:24 -0700

    Upload Files

---
+++ README.md
@@ -0,0 +1,77 @@
+#An edited Code Vulnerability Dataset Generator
+
+# A C/C++ Code Vulnerability Dataset with Code Changes and CVE Summaries
+
+## 1 . Data Description
+
+- CVE entries in our dataset cover the period from 2002 to 2019, each consisting of 21 features. Each feature's name and corresponding column name in the CSV file are explained in the following table. The dataset is released as comma-separated values(CSV) format ([all_c_cpp_release2.0.csv](https://github.com/ZeoVan/MSR_20_Code_Vulnerability_CSV_Dataset/blob/master/all_c_cpp_release2.0.csv)).
+
+|           Features           |    Column Name in the CSV     |                         Description                          |
+| :--------------------------: | :---------------------------: | :----------------------------------------------------------: |
+|      Access Complexity       |      access\_complexity       | Reflects the complexity of the attack required to exploit the software feature misuse vulnerability |
+|   Authentication Required    |   authentication\_required    |  If authentication is required to exploit the vulnerability  |
+|     Availability Impact      |     availability\_impact      | Measures the potential impact to availability of a successfully exploited misuse vulnerability |
+|          Commit ID           |          commit\_id           |   Commit ID in code repository, indicating a mini-version    |
+|        Commit Message        |        commit\_message        |                Commit message from developer                 |
+|    Confidentiality Impact    |    confidentiality\_impact    | Measures the potential impact on confidentiality of a successfully exploited misuse vulnerability |
+|            CWE ID            |            cwe\_id            |                Common Weakness Enumeration ID                |
+|            CVE ID            |            cve\_id            |           Common Vulnerabilities and Exposures ID            |
+|           CVE Page           |           cve\_page           |            CVE Details web page link for that CVE            |
+|         CVE Summary          |            summary            |                   CVE summary information                    |
+|          CVSS Score          |             score             |    The relative severity of software flaw vulnerabilities    |
+|        Files Changed         |        files\_changed         |       All the changed files and corresponding patches        |
+|       Integrity Impact       |       integrity\_impact       | Measures the potential impact to integrity of a successfully exploited misuse vulnerability\ |
+|    Mini-version After Fix    |      version\_after\_fix      |                Mini-version ID after the fix                 |
+|   Mini-version Before Fix    |     version\_before\_fix      |                Mini-version ID before the fix                |
+|     Programming Language     |             lang              |                 Project programming language                 |
+|           Project            |            project            |                         Project name                         |
+|         Publish Date         |         publish\_date         |                   Publish date of the CVE                    |
+|        Reference Link        |           ref\_ink            |                Reference link in the CVE page                |
+|         Update Date          |         update\_date          |                    Update date of the CVE                    |
+| Vulnerability Classification | vulnerability\_classification |                      Vulnerability type                      |
+
+- We used the code changes information(minned from commited version patches) to localize which lines of code in the files were modified. Taking modified lines between the two mini-versions as flaw lines, we split the functions in the modified files into vulnerable functions (if there were flaw lines modified in the function) and non-vulnerable functions.
+
+- ~~The cleaned version of split functions is coming soon.~~
+
+- The cleaned version of split functions(CSV format) can be found at: https://drive.google.com/file/d/1-0VhnHBp9IGh90s2wCNjeCMuy70HPl8X/view?usp=sharing  
+
+  
+
+  Some of the intermediary files in the data collecting and cleaning process can be found at:https://drive.google.com/file/d/1E95oVDSO0CfAAs-Q0yav2HBGLZQce_v2/view?usp=sharing
+  
+  
+  
+  Pandas is recommended (Python Package, like `pandas.read_csv("filepath/file.csv")` ) to work on the dataset.
+  
+  
+  
+  Also JSON format dataset can be found at: https://drive.google.com/file/d/1deNsPfeh77h1SHjJURYOeyCR96JgxB_A/view?usp=sharing
+  
+  - Split functions are released as comma-separated values(CSV) format:
+  
+  | Column Name in the CSV |                         Description                          |
+  | :--------------------: | :----------------------------------------------------------: |
+  |      func_before       | The function before the vulnerability being fixed(if "vul" labeled as "1", then this is the vulnerable function) |
+  |       func_after       |       The function after the vulnerability being fixed       |
+  |      lines_before      | The modified lines in the founction before the vulnerability being fixed |
+  |      lines_after       | The modified lines in the founction after the vulnerability being fixed |
+  |          vul           | "1" means vulnerable function and "0" means non-vulnerable function |
+  |   vul_func_with_fix    |  The code comments showing how the vulnerability was fixed   |
+## 2. HOW To Use The Scripts
+
+- Pre-Requirements
+    - [Python3](https://www.linuxbabe.com/ubuntu/install-python-3-6-ubuntu-16-04-16-10-17-04)
+  - [Beautiful Soup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup)
+    - [Pandas](https://pandas.pydata.org/getting_started.html)
+  
+  - How to use
+    - First use [scrape_all_the_cve.py](https://github.com/ZeoVan/MSR_20_Code_Vulnerability_CSV_Dataset/blob/master/scripts/scrape_all_the_cve.py) to scrape all the CVE entries on CVE Details
+    - Then use [get_commit_info](https://github.com/ZeoVan/MSR_20_Code_Vulnerability_CSV_Dataset/blob/master/scripts/get_commit_info.py) to get commit messages
+    - Finally crawl down all the source files and patch files using commit messages above, and then split all the functions in the modified files, see [all_cpp_c_project_with_chrome_android.ipynb](https://github.com/ZeoVan/MSR_20_Code_Vulnerability_CSV_Dataset/blob/master/notebooks/all_cpp_c_project_with_chrome_android.ipynb) for details.
+  
+  ## Citation
+  
+  ACM Reference Format:
+  
+  Jiahao Fan, Yi Li, Shaohua Wang and Tien N. Nguyen. 2020. A C/C++ Code Vulnerability Dataset with Code Changes and CVE Summaries. In MSR ’20: The 17th International Conference on Mining Software Repositories,May 25–26, 2020, MSR, Seoul, South Korea. ACM, New York, NY, USA, 5 pages. https://doi.org/10.1145/3379597.3387501
---
+++ scripts/get functions.py
@@ -0,0 +1,309 @@
+from calendar import c
+import numpy as np
+import pandas as pd
+import os
+import sys
+import string
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+import time
+import datetime
+import ssl
+import numpy as np
+import pandas as pd
+import traceback
+import csv
+ssl._create_default_https_context = ssl._create_unverified_context
+import json
+import subprocess
+import glob
+import requests
+from pprint import pprint
+from difflib import Differ
+from pathlib import Path
+import pandas
+
+# find all the line numbers that the functions begins
+def get_line_numbers(filename):
+    pos = filename.rfind('.') #position of last period
+    lang_type = filename[pos+1:] #language of file
+    if lang_type in ["C","cc","cxx","cpp","c++","Cpp"]:
+        lang_type = "c++"
+    if (lang_type == "h"):
+        lang_type = "c"
+    if (lang_type != "c++" and lang_type != "c"):
+        return [], -1
+    # found = False
+    #cmd = "ctags -x --c-kinds=fp " + filename + " | grep " + funcname
+    cmd = "ctags -x --"+lang_type+"-kinds=f " + filename
+
+    output = subprocess.getoutput(cmd)
+    lines = output.splitlines()
+    line_nums = []
+    for line in lines:
+        line = line.split(" ")
+        char = list(filter(None, line))
+        line_num = char[2]
+        line_nums.append(int(line_num))
+    return line_nums, 0
+
+def get_diff_lines(filename):
+    diff_lines = []
+    with open(filename, "r") as f:
+        for i, line in enumerate(f):
+            if line.startswith("+") or line.startswith("-"):
+                diff_lines.append(i)
+    return diff_lines
+
+#get code of function starting at line number
+def process_file(filename, line_num):
+    print("opening " + filename + " on line " + str(line_num))
+
+    code = ""
+    cnt_braket = 0
+    found_start = False
+    found_end = False
+
+    with open(filename, "r") as f:
+        for i, line in enumerate(f):
+            if(i >= (line_num - 1)):
+                code += line
+
+                if (not line.startswith("//")) and line.count("{") > 0:
+                    found_start = True
+                    cnt_braket += line.count("{")
+
+                if (not line.startswith("//")) and line.count("}") > 0:
+                    cnt_braket -= line.count("}")
+
+                if cnt_braket == 0 and found_start == True:
+                    found_end = True
+                    return code, i+1
+    if (found_end == False):
+        return "did not find end", -1
+
+def getDiffFromCommit(sha, tries=0): #returns 0 for success, -1 for failure
+    try:
+        curpath = os.path.abspath(os.curdir)
+        print("getting diff for " + sha)
+
+        #get new file
+        url = f"https://api.github.com/search/commits?q=" + sha
+        d = Differ() #create differ
+        commit_url = requests.get(url, 
+            headers={
+                'Authorization': "" Enter GitHub Token Here
+            }
+        ).json()["items"][0]["url"]
+        commit = requests.get(commit_url, 
+            headers={
+                'Authorization': "" Enter GitHub Token Here
+            }
+            ).json()
+        old_commit_sha = commit["parents"][0]["sha"] #sha of parent
+        num_changed_files = len(commit["files"]) #number of files changed in commit
+        for i in range(num_changed_files):
+            #get new file
+            raw_url = commit["files"][i]["raw_url"]
+            pos = raw_url.rfind('.') #position of last period
+            lang = raw_url[pos+1:] #language of file
+            new_file = requests.get(raw_url).text #postcommit in string format
+            with open(os.path.join(curpath, "temp_files\\new_file" + str(i) + "." + lang), "w", encoding="utf-8") as new_file_temp: #save postcommit file
+               new_file_temp.write(new_file)
+
+            #get old file
+            old_raw_url = raw_url.replace(sha, old_commit_sha)
+            old_file = requests.get(old_raw_url).text #precommit file in string format
+            with open(os.path.join(curpath, "temp_files\old_file" + str(i) + "." + lang), "w", encoding="utf-8") as old_file_temp: #save precommit file
+                old_file_temp.write(old_file)
+
+            #diff the files
+
+            with open (os.path.join(curpath, "temp_files\old_file" + str(i) + "." + lang), encoding="utf-8") as f:
+                file1_lines = f.readlines()
+            with open (os.path.join(curpath, "temp_files\\new_file" + str(i) + "." + lang), encoding="utf-8") as f:
+                file2_lines = f.readlines()
+            difference = list(d.compare(file1_lines, file2_lines))
+            difference = '\n'.join(difference)
+            with open(os.path.join(curpath, "diff_files\diff" + str(i) + "." + lang), "w", encoding="utf-8") as diff_file:
+                diff_file.write(difference)
+
+        return 0, ""
+    except HTTPError as e:
+        if e.code == 429:
+            if (tries >= 5):
+                print("429 please wait")
+                print(url)
+                return -1, "429"
+            else:
+                time.sleep(10)
+                return getDiffFromCommit(sha, tries+1)
+        if e.code == 404:
+            print("\n not found:" + url+ "！")
+            return -1, "404"
+        if e.code == 403:
+            if (tries >= 5):
+                print("403 please wait")
+                print(url)
+                return -1, "403"
+            else:
+                time.sleep(10)
+                return getDiffFromCommit(sha, tries+1)
+        raise
+    except Exception as e:
+        if (tries >= 5):
+            traceback.print_exc(file=sys.stdout)
+            print("reason", e)
+            print("\n skip get_response:"+sha+ "！")
+            return -1, e
+        else:
+            time.sleep(10)
+            return getDiffFromCommit(sha, tries+1)
+
+def main():
+    curpath = os.path.abspath(os.curdir)
+    logdir = os.path.join(curpath, "Logs")
+    log = open(os.path.join(logdir, "function_grabbing_log.txt"), "w")
+    commit_list = pandas.read_csv(os.path.join(curpath, "data/commits.csv"))
+    error_output_list = open(os.path.join(curpath, "output/errorCommits.csv"), "w", encoding='UTF8')
+    error_writer=csv.writer(error_output_list)
+    error_writer.writerow(['CWE ID', "commit ID", "reason"])
+
+    with open(os.path.join(curpath, "output/output.csv"), "w", encoding='UTF8') as output:
+        writer = csv.writer(output)
+        writer.writerow(['CWE ID', 'commit ID', 'func_before', 'processed_func', 'vul_func_with_fix'])
+
+        for index, row in commit_list.iterrows():
+            try:
+                commitID = row["commit ID"]
+                if (commitID == "" or commitID == "nan"):
+                    continue
+                CWEID = row["CWE ID"]
+                if (CWEID == ""):
+                    error_writer.writerow(["",commitID, "no CWE id"])
+                    continue
+                result, errormsg = getDiffFromCommit(commitID)
+                if (result == 0):
+                    diffdir = os.path.join(curpath, "diff_files")
+
+                    filenum = 0
+                    for filename in os.listdir(diffdir):
+                        f = os.path.join(diffdir, filename)
+                        # checking if it is a file
+                        if os.path.isfile(f):
+                            pos = f.rfind('.') #position of last period
+                            lang = f[pos+1:] #language of file            
+                            #get function line numbers in diff
+                            func_line_nums, msg = get_line_numbers(f)
+                            if (msg == -1):
+                                error_writer.writerow([CWEID, commitID, "unknown language"])
+                            func_line_nums.sort()
+                            #get function line numbers in precommit file
+                            print("oldfile lang " + lang)
+                            print(filenum)
+                            oldfile = os.path.join(curpath, "temp_files\old_file" + str(filenum) + "." + lang)
+                            old_func_line_nums, msg = get_line_numbers(oldfile)
+                            if (msg == -1):
+                                error_writer.writerow([CWEID, commitID, "unknown language"])
+                            old_func_line_nums.sort()
+                            print(old_func_line_nums)
+                            #get function line numbers in postcommit file
+                            newfile = os.path.join(curpath, "temp_files\\new_file" + str(filenum) + "." + lang)
+                            new_func_line_nums, msg = get_line_numbers(newfile)
+                            if (msg == -1):
+                                error_writer.writerow([CWEID, commitID, "unknown language"])
+                            new_func_line_nums.sort()
+
+                            filenum != 1
+
+                            #if the number of functions between the old and new file are different
+                            #some functions are added/removed instead of simply replaced
+                            #which requires more work, and will be put aside for later.
+                            if (len(new_func_line_nums) != len(old_func_line_nums)):
+                                log.write("commit has changed the number of functions: " + commitID + "\n")
+                                error_writer.writerow([CWEID, commitID, "changed num of functions"])
+                                continue
+
+                            #get diff line numbers
+                            diff_lines = get_diff_lines(f)
+
+                            #get changed functions
+                            changed_funcs_lines = []
+                            changed_funcs_index = []
+                            i=0
+                            j=0
+                            while (i<len(func_line_nums)-1 and j<len(diff_lines)):
+                                if ((diff_lines[j] < func_line_nums[i+1] and diff_lines[j] > func_line_nums[i]) or diff_lines[j] == func_line_nums[i]):
+                                    changed_funcs_lines.append(func_line_nums[i])
+                                    changed_funcs_index.append(i)
+                                    i+=1
+                                    j+=1
+                                elif (diff_lines[j] < func_line_nums[i]):
+                                    j+=1
+                                elif (diff_lines[j] > func_line_nums[i]):
+                                    i+=1
+
+                            if (len(changed_funcs_lines) == 0):
+                                log.write("no functions were changed in commit: " + commitID)
+                                error_writer.writerow([CWEID, commitID, "no functions changed"])
+                                continue
+
+                            #replace + and - in function
+                            func_num = 0
+                            for func_line in changed_funcs_lines:
+                                #get function code
+                                code, end_num = process_file(f, func_line)
+                                print("opened diff file")
+                                #write it to a temp file
+                                with open(os.path.join(curpath, "temp_files/temp_changed_func.txt"), "w") as temp:
+                                    temp.write(code)
+
+                                #replace the lines
+                                with open(os.path.join(curpath, "temp_files/temp_changed_func.txt"), "r") as temp:
+                                    #write to new file
+                                    with open(os.path.join(curpath, "temp_files/temp_changed_func_edited.txt"), "w") as temp_changed:
+                                        #get code lines of original function
+                                        for i,line in enumerate(temp):
+                                            #check if code is empty
+                                            if (line != "+ \n" and line != "- \n"):
+                                                #replace code
+                                                if line.startswith("+"):
+                                                    line = line.replace("+","//fix_flaw_line_below:\n//",1)
+                                                if line.startswith("-"):
+                                                    line = line.replace("-","//flaw_line_below:\n",1)
+                                                if line.startswith("?"):
+                                                    continue
+                                                if not code.endswith("\n"):
+                                                    line = line + "\n"
+                                                #output to new file
+                                                temp_changed.write(line)
+
+                                #read edited file
+                                with open(os.path.join(curpath, "temp_files/temp_changed_func_edited.txt"), "r") as temp_changed:
+                                    edited_code = temp_changed.read() #vul_func_with_fix
+
+                                print("getting function number " + str(func_num))
+                                #get func_before
+                                print(oldfile)
+                                print(old_func_line_nums)
+                                func_before, i = process_file(oldfile, old_func_line_nums[changed_funcs_index[func_num]])
+
+                                #get func_after/processed_func
+                                func_after, i = process_file(newfile, new_func_line_nums[changed_funcs_index[func_num]])
+                                
+                                #write to csv
+                                writer.writerow([CWEID, commitID, func_before, func_after, edited_code])
+
+                                func_num += 1
+                elif (result == -1):
+                    log.write("error retriving commit info for ID: " + commitID + " because of " + str(errormsg) + "\n")
+                    error_writer.writerow([CWEID, commitID, errormsg])
+            except Exception as e:
+                print(str(e))
+                error_writer.writerow([CWEID, commitID, str(e)])
+    
+    log.close()
+
+if __name__ == "__main__":
+    main()
